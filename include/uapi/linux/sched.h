87b0ce3e8a470b6472a0634fe70cbbb2db9c607f
Elias Carter <edcarter@ualberta.ca>
2017-06-24 17:23:40 -0700

    add SCHED_BOGO policy id macro

--- include/uapi/linux/sched.h
+++ include/uapi/linux/sched.h
@@ -39,6 +39,9 @@
 /* SCHED_ISO: reserved but not implemented yet */
 #define SCHED_IDLE		5
 #define SCHED_DEADLINE		6
+#ifdef CONFIG_SCHED_BOGO_POLICY
+#define SCHED_BOGO              7
+#endif
 
 /* Can be ORed in to make sure the process is reverted back to SCHED_NORMAL on fork */
 #define SCHED_RESET_ON_FORK     0x40000000
