1ec4dd888cfcd2c0c2a639572b71108ec6f3c3e9
Elias Carter <edcarter@ualberta.ca>
2017-06-27 22:34:08 -0700

    added method stubs for bogo schedule class

--- kernel/sched/bogo.c
+++ kernel/sched/bogo.c
@@ -1,5 +1,78 @@
+#include "sched.h"
+
+/*
+ * stop-task scheduling class.
+ *
+ * The stop task is the highest priority task in the system, it preempts
+ * everything and will be preempted by nothing.
+ *
+ * See kernel/stop_machine.c
+ */
+
+static void
+enqueue_task_bogo(struct rq *rq, struct task_struct *p, int flags)
+{
+}
+
+static void
+dequeue_task_bogo(struct rq *rq, struct task_struct *p, int flags)
+{
+}
+
+static void yield_task_bogo(struct rq *rq)
+{
+}
+
+static void
+check_preempt_curr_bogo(struct rq *rq, struct task_struct *p, int flags)
+{
+}
+
+static struct task_struct *
+pick_next_task_bogo(struct rq *rq, struct task_struct *prev, struct rq_flags *rf)
+{
+}
+
+static void put_prev_task_bogo(struct rq *rq, struct task_struct *prev)
+{
+}
+
+#ifdef CONFIG_SMP
+static int
+select_task_rq_bogo(struct task_struct *p, int cpu, int sd_flag, int flags)
+{
+}
+#endif /* CONFIG_SMP */
+
+static void set_curr_task_bogo(struct rq *rq)
+{
+}
+
+static void task_tick_bogo(struct rq *rq, struct task_struct *curr, int queued)
+{
+}
+
+static unsigned int
+get_rr_interval_bogo(struct rq *rq, struct task_struct *task)
+{
+}
+
+static void
+prio_changed_bogo(struct rq *rq, struct task_struct *p, int oldprio)
+{
+}
+
+static void switched_to_bogo(struct rq *rq, struct task_struct *p)
+{
+}
+
+static void update_curr_bogo(struct rq *rq)
+{
+}
+
 const struct sched_class bogo_sched_class = {
 	.next			= &rt_sched_class,
+
 	.enqueue_task		= enqueue_task_bogo,
 	.dequeue_task		= dequeue_task_bogo,
 	.yield_task		= yield_task_bogo,
@@ -11,12 +84,7 @@ const struct sched_class bogo_sched_class = {
 
 #ifdef CONFIG_SMP
 	.select_task_rq		= select_task_rq_bogo,
-
 	.set_cpus_allowed       = set_cpus_allowed_common,
-	.rq_online              = rq_online_bogo,
-	.rq_offline             = rq_offline_bogo,
-	.task_woken		= task_woken_bogo,
-	.switched_from		= switched_from_bogo,
 #endif
 
 	.set_curr_task          = set_curr_task_bogo,
