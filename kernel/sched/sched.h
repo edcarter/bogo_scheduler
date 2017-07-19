2dc110a2d31ca2c927b09e9b16119029982161a7
Elias Carter <edcarter@ualberta.ca>
2017-07-19 16:16:20 -0700

    add bogo policy to valid_policy

--- kernel/sched/sched.h
+++ kernel/sched/sched.h
@@ -148,7 +148,8 @@ static inline int dl_policy(int policy)
 static inline bool valid_policy(int policy)
 {
 	return idle_policy(policy) || fair_policy(policy) ||
-		rt_policy(policy) || dl_policy(policy);
+		rt_policy(policy) || dl_policy(policy) ||
+		bogo_policy(policy);
 }
 
 static inline int task_has_rt_policy(struct task_struct *p)
